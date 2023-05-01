95f89c8095172e3e5e96a189aec9b6b5fedbbe0f
Akshay Suhag <83011261+aa20g217@users.noreply.github.com>
2023-05-01 18:30:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,50 @@ from UI.uploadInput import upload_data_content
 from UI.autoML import autoML_content
 
 
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!    
+navigation_bar = dbc.Navbar(
+    dbc.Container(        [            
+            #dbc.Col(dbc.NavbarBrand(html.Img(src="assets/logo.svg", height="50px")),width=2),
+            dbc.Col(dbc.NavbarBrand(html.I("TOOL NAME"), style={"margin-left": "0px","font-weight": "bold","font-size": "40px","color":"white"}),width=1,align="left"),
+            dbc.Col(width=9),
+            html.A(dbc.Col(html.Img(src="https://www.unibe.ch/media/logo-unibern-footer@2x.png", height="80px"),align="right"),
+                href="https://www.unibe.ch/index_ger.html", target="_blank",
+                style={"textDecoration": "none"}),
+        ],fluid=True,
+    ),
+   color="#444", 
+   dark=True,style={"border-color": "white"}
+)
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+about=dbc.Card([html.Div([
+    dbc.Row(html.H5("TOOL",style={"font-weight": "bold","color":"white"})),
+    dbc.Row(html.P("Machine learning (ML) has become an essential tool for researchers to analyze and extract valuable insights from complex datasets. However, creating an effective ML pipeline can be a daunting task, especially for researchers who lack technical proficiency or expertise in this field. Even researchers who possess technical proficiency needs to invest significant time and effort in developing an ML pipeline. To address these challenges, we have developed a tool called ----tool name----.",style={"text-align": "justify"})),
+    html.Br(),
+    dbc.Row(html.P("----tool name---- empowers researchers to utilize machine learning techniques in their research, regardless of their coding and technical skills. The tool provides four primary functionalities: data exploration, auto ML, custom ML, and visualization. Users can examine their datasets and obtain initial insights through an intuitive interface with the data exploration feature. With the auto-ML feature, users can leverage a pre-built ML pipeline without requiring technical expertise. The custom ML interface is intended for advanced users and provides a user-friendly platform for developing tailor-fit ML pipelines to meet their specific research needs. Finally, Users can interpret and analyze their findings effortlessly with the help of various tables and plots using the visualization feature.",style={"text-align": "justify"})),
+    html.Br(),
+    dbc.Row(html.H5("Availability",style={"font-weight": "bold","color":"white"})),
+    html.Div(["TOOL is developed by the ",
+             html.A("Functional Urology group", href="http://www.urofun.ch/", target="_blank"),
+             " at the ",
+             html.A("University of Bern", href="https://www.unibe.ch/index_ger.html", target="_blank"),
+             ". The source code and tutorial can be found on the ",
+             html.A("TOOL GitHub repository", href="https://github.com/FunctionalUrology/", target="_blank"),
+    ],style={"text-align": "justify"}),
+    
+    html.Br(),
+    dbc.Row(html.H5("Contact",style={"font-weight": "bold","color":"white"})),
+    html.P("Bug reports and new feature requests can be communicated via:"),
+    html.Ul([html.Li(html.Div(["GitHub : ",html.A("https://github.com/FunctionalUrology/", href="https://github.com/FunctionalUrology/", target="_blank")]),)]),
+    html.Ul([html.Li("Email : akshay.akshay@unibe.ch , ali.hashemi@dbmr.unibe.ch")]),
+    html.Br(),
+    dbc.Row(html.H5("Citation",style={"font-weight": "bold","color":"white"})),
+    html.Div(["If TOOL helps you in any way, please cite the TOOL article:"]),
+    html.Ul([html.Li(html.A("", href="", target="_blank"))]),
+
+    ],style={"margin-left": "50px","margin-right": "50px","margin-top": "30px","font-size": "14px"})],
+    style={"margin-left": "2px","margin-right": "2px","margin-top": "5px"})
+
 
 
 app.css.config.serve_locally = False
@@ -43,7 +87,7 @@ tabs = dbc.Card(
             card=True,
         )),
         dbc.CardBody(html.P(id="content", className="mt-3")),
-    ],className="mt-3",color="secondary", outline=False)
+    ],className="mt-3",color="grey", outline=False)
 
 
 tabs_main = dbc.Card(
@@ -54,19 +98,21 @@ tabs_main = dbc.Card(
                 dbc.Tab(label="Data Exploration", tab_id="upload_data"),
                 dbc.Tab(label="Auto ML", tab_id="autoML"),
                 dbc.Tab(label="Custom ML", tab_id="customML"),
-                dbc.Tab(label="Visualisation", tab_id="result")
+                dbc.Tab(label="Visualisation", tab_id="result"),
+                dbc.Tab(label="About", tab_id="about")
+
             ],
             id="tabs_main",
             active_tab="upload_data",
             card=True,
         )),
         dbc.CardBody(html.P(id="content_main", className="mt-3")),
-    ],className="mt-3",color="secondary", outline=False)
+    ],className="mt-3",color="grey", outline=False)
 
 
 
 
-app.layout = html.Div([
+app.layout = html.Div([navigation_bar,
     dcc.Store(id='scaling_tab_data',data={}),
     dcc.Store(id='overSamp_tab_para',data={}),
     dcc.Store(id='underSamp_tab_para',data={}),
@@ -113,6 +159,9 @@ def switch_tab(at):
         return tabs
     elif at == "result":
         return uploadResult_content
+    
+    elif at == "about":
+        return about 
     return html.P("This shouldn't ever be displayed...")
 
 
