3bcee1a39461b4907cbb7a18901eb59c9087512c
Akshay Suhag <83011261+aa20g217@users.noreply.github.com>
2023-05-01 18:32:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ Created on Thu Sep  9 16:29:02 2021
 
 import dash
 import dash_bootstrap_components as dbc
-
+#DARKLY
 
 external_stylesheets=[dbc.themes.DARKLY,
                       'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css']
